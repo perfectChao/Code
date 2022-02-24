849cf2c8d0ee87c65da6ae5402e828a80f80a375
xuchao <xuchao@yuanfudao.com>
2022-02-24 18:47:46 +0800

    add code 2.24

--- Code/main.cpp
+++ Code/main.cpp
@@ -17,18 +17,35 @@ template <typename T> bool chkMin(T &x, T y) { return (y < x) ? x = y, 1 : 0; }
 
 int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
 
-//class Solution {
-//public:
-//    int numberOfGoodSubsets(vector<int>& nums) {
-//        return 0;
-//    }
-//};
+class Solution {
+public:
+    int L = -1, R = 1;
+    int m, n;
+    
+    int dfs(int x, int y, vector<vector<int>>& g) {
+        int nx = x + 1;
+        int ny = g[x][y] == L ? y - 1 : y + 1;
+        if (ny < 0 || ny >= n) return -1;
+        if (g[x][y] != g[x][ny]) return -1;
+        if (x == m - 1) return y;
+        return dfs(nx, ny, g);
+    }
+    
+    vector<int> findBall(vector<vector<int>>& grid) {
+        m = grid.size(), n = grid[0].size();
+        vector<int> ans;
+        for (int i = 0, j = 0; j < n; j++) {
+            ans.push_back(dfs(i, j, grid));
+        }
+        return ans;
+    }
+};
 
 int main() {
-//    Solution s;
-    
-    get_primes(30);
-    
+    print_code_s("[[1,1,1,1,1,1],[-1,-1,-1,-1,-1,-1],[1,1,1,1,1,1],[-1,-1,-1,-1,-1,-1]");
+    vector<vector<int>> vv = {{1,1,1,1,1,1},{-1,-1,-1,-1,-1,-1},{1,1,1,1,1,1},{-1,-1,-1,-1,-1,-1}};
+    Solution s;
+    print_code_arr(s.findBall(vv));
     return 0;
 }
 
