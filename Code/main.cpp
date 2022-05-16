e25a549c86967387302518cd949a85affc36f759
stefanie <xuchao@ksjgs.com>
2022-05-16 11:19:59 +0800

    add dx dy

--- Code/main.cpp
+++ Code/main.cpp
@@ -17,62 +17,64 @@ template <typename T> bool chkMin(T &x, T y) { return (y < x) ? x = y, 1 : 0; }
 
 class Solution {
 public:
-    vector<int> presum;
-    vector<int> arr;
     
-    int cal(string& s, vector<int>& q) {
-        if (arr.size() == 0) return 0;
-        
-        int lo = q[0], hi = q[1];
-        int left = -1, right = -1;
-        
-        int l = 0, r = arr.size() - 1;
-        while (l < r) {
-            int mid = l + r >> 1;
-            if (arr[mid] >= lo) r = mid;
-            else l = mid + 1;
-        }
-        if (arr[r] >= lo) left = arr[r];
-        else return 0;
-        
-        l = 0, r = arr.size() - 1;
-        while (l < r) {
-            int mid = l + r + 1 >> 1;
-            if (arr[mid] <= hi) l = mid;
-            else r = mid - 1;
-        }
-        if (arr[r] <= hi) right = arr[r];
-        else return 0;
-        
-        if (left < right) return presum[right + 1] - presum[left];
-        return 0;
-    }
-    
-    vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
-        int n = s.size();
-        presum.resize(n + 1);
-        for (int i = 1; i <= n; i++) {
-            int x = s[i - 1] == '*' ? 1 : 0;
-            if (x == 0) arr.pb(i - 1);
-            presum[i] = presum[i - 1] + x;
-        }
-        
+    int curTime = 0;
+    vector<int> busiestServers(int k, vector<int>& arrival, vector<int>& load) {
+        priority_queue<int, vector<int>, greater<int>> s_pq;
+        for (int i = 0; i < k; i++) s_pq.push(i);
+
+        auto cmp = [&](auto p1, auto p2) {
+            return p1.second > p2.second;
+        };
+        priority_queue<PII, vector<PII>, decltype(cmp)> pq(cmp);
+
         vector<int> ans;
-        for (auto& q : queries) {
-            ans.pb(cal(s, q));
+        unordered_map<int, int> cnt;
+        int mxCnt = 0;
+
+        int pos = 0;
+        int n = arrival.size();
+        while (pos < n) {
+            int now = arrival[pos], spend = load[pos];
+            curTime = now;
+
+            while (pq.size()) {
+                auto t = pq.top();
+                if (t.second - curTime <= 0) {
+                    pq.pop();
+                    s_pq.push(t.first);
+                    continue;
+                }
+                break;
+            }
+
+            if (s_pq.size()) {
+                int s_no = s_pq.top(); s_pq.pop();
+                pq.push({s_no, now + spend});
+                cnt[s_no]++;
+                if (cnt[s_no] > mxCnt) {
+                    mxCnt = cnt[s_no];
+                    ans.clear();
+                    ans.push_back(s_no);
+                } else if (cnt[s_no] == mxCnt) {
+                    ans.push_back(s_no);
+                }
+            }
+
+            pos++;
         }
+
         return ans;
     }
 };
 
 int main() {
 //    print_code_s("[[1,17],[4,5],[14,17],[5,11],[15,16]]");
-//    vector<int> v = {1,2,3};
-    vector<vector<int>> vv = {{2,2}};
+    vector<int> v = {1,2,3,4,8,9,10};
+    vector<int> v1 = {5,2,10,3,1,2,2};
+    vector<vector<int>> vv = {{1,3},{6,9}};
     Solution s;
-    for (int x : s.platesBetweenCandles("***", vv)) {
-        cout << x << endl;
-    }
+    vector<int> ans = s.busiestServers(3, v, v1);
 
     return 0;
 }
